17f19d90e70fc0bec765431f8f7cf05237ffa7ca
And4Web <anands2807@gmail.com>
2022-05-10 16:35:29 +0530

    firebase installed

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import firestore from "";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyXkF49sT0NYWC3Fx4XZX_b1vb6_0tmVo",
+  authDomain: "testtask-42331.firebaseapp.com",
+  projectId: "testtask-42331",
+  storageBucket: "testtask-42331.appspot.com",
+  messagingSenderId: "238628593954",
+  appId: "1:238628593954:web:808c833ecac8b26f47c57b",
+  measurementId: "G-8Q6PLXZ3V0",
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
