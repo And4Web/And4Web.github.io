5eb4abfe7aab9c6ae1d5480659fc641391729953
And4Web <anands2807@gmail.com>
2022-05-10 09:16:32 +0530

    creating two more sectin on the page

--- src/pages/homePage/Home.jsx
+++ src/pages/homePage/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import Header from "../../components/header/Header";
+import banner from "../../images/banner.png";
+import "./home.css";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <img src={banner} alt="banner" className="banner" />
+
+      <div className="intro">
+        <h2 className="mainTitle">How it works</h2>
+        <p> sea basket delivers fresh sourced seafood in a few easy clicks</p>
+      </div>
+
+      <div className="categories">
+        <h3 className="mainTitle">Categories</h3>
+        <div className="test"></div>
+      </div>
+
+      <div className="glance">
+        <h2 className="mainTitle">a glance at our product</h2>
+        <div className="video-container">{/* <video></video> */}</div>
+      </div>
+
+      <div className="faqs"></div>
     </div>
   );
 }
